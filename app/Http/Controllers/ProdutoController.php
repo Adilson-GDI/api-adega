233ac73bffe1797439598213cc175cf4df95372c
Adilson Pina <adilson.gditecnologia@gmail.com>
2022-07-11 16:55:48 -0300

    ajuste

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -7,12 +7,18 @@ use App\Models\Produto;
 
 class ProdutoController extends Controller
 {
-    public function list(){
+    public function list(Request $request){
 
-        $produto=Produto::all();
-        return $produto;
+      $produto=Produto::where('id_categoria',$request->get("categoria"))
+      ->get(); 
+        return $produto;  
+        
+       /*  $produto=Produto::all();
+        return $produto;   */
 
     }
+
+
     public function show($id){
 
         $produto = Produto::find($id);
