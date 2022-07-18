dfe77f68f6e64be1ece97e9294af0c6d1ccb6ae5
Adilson Pina <adilson.gditecnologia@gmail.com>
2022-07-17 21:04:51 -0300

    Ajuste

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::get('/', function()
     return "ola";
 });
 
+Route::get('/produtos/', [App\Http\Controllers\ProdutoController::class, 'list'])->name('list');
 Route::get('/produto/', [App\Http\Controllers\ProdutoController::class, 'list'])->name('list');
 
 
